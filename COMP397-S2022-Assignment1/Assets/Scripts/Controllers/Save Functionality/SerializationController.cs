3cd43baa76636f15619f1acc9918aa32877a344b
Sukhmannat Singh <thegamingtitan17@gmail.com>
2022-06-26 00:16:59 -0400

    [ADD] Save Functionality

---
+++ COMP397-S2022-Assignment1/Assets/Scripts/Controllers/Save Functionality/SerializationController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SerializationController
+{
+    public static bool Save(string saveName, object saveData)
+    {
+        BinaryFormatter formatter = GetBinaryFormatter();
+
+        if (!Directory.Exists(Application.persistentDataPath + "/saves"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/saves");
+        }
+
+        string path = Application.persistentDataPath + "/saves/" + saveName + ".save";
+
+        FileStream file = File.Create(path);
+
+        formatter.Serialize(file, saveData);
+        file.Close();
+
+        return true;
+    }
+
+    public static object Load(string path)
+    {
+        if (!File.Exists(path))
+        {
+            return null;
+        }
+
+        BinaryFormatter formatter = GetBinaryFormatter();   
+
+        FileStream file = File.Open(path, FileMode.Open); 
+
+        try
+        {
+            object data = formatter.Deserialize(file);
+            file.Close();
+            return data;
+        }
+        catch
+        {
+            Debug.LogError("Error");
+            file.Close();
+            return null;
+        }
+    }
+
+     public static BinaryFormatter GetBinaryFormatter()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        return formatter;
+    }
+}
