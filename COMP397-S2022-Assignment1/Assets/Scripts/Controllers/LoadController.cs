788e22ce77740100f2e0c928a54c4fce328758cb
Sukhmannat Singh <thegamingtitan17@gmail.com>
2022-08-05 15:00:08 -0400

    [MOD] Load Button on Start Screen works

---
+++ COMP397-S2022-Assignment1/Assets/Scripts/Controllers/LoadController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+
+public class LoadController : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject loadDataButton1;
+    [SerializeField] private GameObject loadDataButton2;
+
+    private string fileName;
+    private bool sceneChange = true;
+    void Update()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (SceneManager.GetActiveScene().name == "Main" && sceneChange)
+        {
+            Debug.Log(1);
+            GameController.instance.OnLoad(fileName);
+            sceneChange = false;
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void PreLoad()
+    {
+        if (File.Exists(Application.persistentDataPath + "/saves/Save 1.save"))
+        {
+            SaveData.current = (SaveData)SerializationController.Load(Application.persistentDataPath + "/saves/Save 1.save");
+
+            loadDataButton1.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Save 1";
+            loadDataButton1.gameObject.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Wave: " + SaveData.current.playerData.wave;
+            loadDataButton1.gameObject.transform.GetChild(1).gameObject.GetComponent<Text>().text = "Player Health: " + SaveData.current.playerData.health;
+            loadDataButton1.gameObject.transform.GetChild(3).gameObject.GetComponent<Text>().text = "Score: " + SaveData.current.playerData.score;
+        }
+
+        if (File.Exists(Application.persistentDataPath + "/saves/Save 2.save"))
+        {
+            SaveData.current = (SaveData)SerializationController.Load(Application.persistentDataPath + "/saves/Save 2.save");
+
+            loadDataButton2.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Save 2";
+            loadDataButton2.gameObject.transform.GetChild(1).gameObject.GetComponent<Text>().text = "Player Health: " + SaveData.current.playerData.health;
+            loadDataButton2.gameObject.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Wave: " + SaveData.current.playerData.wave;
+            loadDataButton2.gameObject.transform.GetChild(3).gameObject.GetComponent<Text>().text = "Score: " + SaveData.current.playerData.score;
+        }
+    }
+
+    public void OnLoad(string file)
+    {
+        fileName = file;
+        Debug.Log(fileName);
+    }
+}
